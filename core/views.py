cc7220668abc0468b63befd95b306354941a8a94
Luca Yan <lucayan0506>
2022-05-18 16:06:51 +0100

    change way of receive post request

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
+from math import fabs
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from . import models
+import json
 
 # Create your views here.
 
@@ -11,10 +13,12 @@ def index_page(request):
 @csrf_exempt
 def send_message(request):
     if request.method == "POST":
-        email = request.POST['email']
-        subject = request.POST['subject']
-        message = request.POST['message']
+        body = json.loads(request.body)
+        email = body['email']
+        subject = body['subject']
+        message = body['message']
         newMessage = models.Message(mail=email, subject=subject, message=message)
         newMessage.save()
+        return JsonResponse("Message sent successfully, thank you for your message. I will reply as soon as possible (make sure that the email is correct, otherwise I cannot reply)",safe=False)
         
     return HttpResponse("Please, send a POST method")
