653559696847d230ab3d656e0319dd3fdb29f66b
Luca Yan <lucayan0506>
2022-12-21 18:37:55 +0000

    hide & change secrete key

--- ownAPI/settings.py
+++ ownAPI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-36%q47mhx()u)!65jlv5admf-!wfuez!fz&l*cmc)-^r$h1_p@'
+SECRET_KEY = '*************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'lucayanwork@gmail.com'
-EMAIL_HOST_PASSWORD = 'peumomelhoqrabgm'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = '**********'
+EMAIL_PORT = '***'
+EMAIL_USE_TLS = '***'
 DEFAULT_FROM_EMAIL = 'lucayanwork@gmail.com'
